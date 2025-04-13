958e5a8bf2c3dad013920ba403f26cdf04f71afc
MKFarag <mohammedkhaled.fa@gmail.com>
2025-04-13 04:48:18 +0200

    Change IsDeleted column of roles table

---
+++ SmartAttendanceSystem.Infrastructure/Persistence/Migrations/20250413024741_ChangeIsDeletedOfRoles.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartAttendanceSystem.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeIsDeletedOfRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "AspNetRoles",
+                newName: "IsDisabled");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDisabled",
+                table: "AspNetRoles",
+                newName: "IsDeleted");
+        }
+    }
+}
